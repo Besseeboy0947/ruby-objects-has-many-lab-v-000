fb7745e796ade6de38bc9290e97d857a8decc146
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-17 03:20:45 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+  attr_accessor :author, :title
+   def initialize(title)
+    @title = title
+  end
+  def self.all
+    @@all
+  end
+   def author_name
+    self.author ? self.author.name : nil
+  end
+end
