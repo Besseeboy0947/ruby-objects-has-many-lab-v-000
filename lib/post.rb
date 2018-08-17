f203c2de1c412c427067bb5565dd9ef7dd926451
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-17 03:21:56 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,13 +6,6 @@ class Post
   def self.all
     @@all
   end
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
- def self.clear_all
-   @@all = []
-end 
   def self.all
     @@all.each do |song|
       puts song.name
