3b04820659b00c8c75663c1bfa8a97967ff477c0
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-08-17 03:22:14 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -6,10 +6,7 @@ class Post
   def self.all
     @@all
   end
-  def self.all
-    @@all.each do |song|
-      puts song.name
-    end
+  
    def author_name
     self.author ? self.author.name : nil
   end
